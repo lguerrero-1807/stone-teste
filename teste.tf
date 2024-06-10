55212ebd4850ffca98b80b6caf46e952974e725a
Lucas Guerrero <lucas.1807@hotmail.com>
2024-06-10 13:58:18 -0300

    feat: addind file that setup the whole system

--- teste.tf
+++ teste.tf
@@ -2,70 +2,43 @@ locals {
   cluster_name  = "stone_teste"
   aws_region    = "us-east-1"
   k8s_version   = "1.30"
-}
-
-data "aws_vpc" "cluster-vpc" {
-  depends_on = [module.network_teste]
-  tags = {
-    Name  = "stone_teste-vpc"
-  }
-}
-
-data "aws_subnet" "private-1a"{
-  depends_on = [module.network_teste]
-  tags = {
-    Name  = "stone_teste-private-1a"
-  }
-}
-
-data "aws_subnet" "private-1c"{
-  depends_on = [module.network_teste]
-  tags = {
-    Name  = "stone_teste-private-1c"
-  }
-}
-
-data "aws_security_group" "eks-sg" {
-  depends_on = [module.eks_teste]
-  tags = {
-    Name  = "stone_teste-master-sg"
-  }
+  cidr_block    = "10.0.0.0/16"
 }
 
 module "network_teste" {
   source = "git::https://github.com/lguerrero-1807/blueprint-modules.git//network"
 
-  cluster_name  = local.cluster_name
-  aws_region    = local.aws_region
+  cluster_name                = local.cluster_name
+  aws_region                  = local.aws_region
+  cidr_block                  = local.cidr_block
+  private_subnet_cidr_blocks  = ["10.0.32.0/20", "10.0.48.0/20"]
+  public_subnet_cidr_blocks   = ["10.0.0.0/20", "10.0.16.0/20"]
+  availability_zones          = ["us-east-1a", "us-east-1c"]
 }
 
 module "eks_teste" {
   depends_on = [module.network_teste]
   source = "git::https://github.com/lguerrero-1807/blueprint-modules.git//eks"
 
-  cluster_name      = local.cluster_name
-  aws_region        = local.aws_region
-  k8s_version       = local.k8s_version
-  cluster_vpc       = data.aws_vpc.cluster-vpc
-  private_subnet_1a = data.aws_subnet.private-1a
-  private_subnet_1c = data.aws_subnet.private-1c
+  cluster_name            = local.cluster_name
+  aws_region              = local.aws_region
+  k8s_version             = local.k8s_version
+  cluster_vpc             = module.network_teste.cluster_vpc
+  private_subnet_ids      = module.network_teste.private_subnets
 }
 
 
 module "nodes_teste" {
+  depends_on = [module.network_teste, module.eks_teste]
   source = "git::https://github.com/lguerrero-1807/blueprint-modules.git//nodes"
 
-  cluster_name          = local.cluster_name
-  aws_region            = "us-east-1"
-  k8s_version           = local.k8s_version
-  cluster_vpc           = data.aws_vpc.cluster-vpc
-  private_subnet_1a     = data.aws_subnet.private-1a
-  private_subnet_1c     = data.aws_subnet.private-1c
-  eks_cluster           = local.cluster_name
-  eks_cluster_sg        = data.aws_security_group.eks-sg
-  #Mudar este nome de variavel para instace_type
-  nodes_instances_sizes = ["t3a.micro"]
-  auto_scale_options    = {
+  cluster_name            = local.cluster_name
+  aws_region              = local.aws_region
+  k8s_version             = local.k8s_version
+  cluster_vpc             = module.network_teste.cluster_vpc
+  private_subnet_ids      = module.network_teste.private_subnets
+  nodes_instances_types   = ["t3a.micro"]
+  auto_scale_options      = {
     desired = 1
     min     = 1
     max     = 3
